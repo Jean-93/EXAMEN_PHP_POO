6527273361e9ed34c56081222f6b30163a32471f
Jean BOURLARD <jean.bourlard@icloud.com>
2020-05-25 14:44:16 +0200

    export_bdd

---
+++ dump.sql
@@ -0,0 +1,135 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:8889
+-- Généré le :  lun. 25 mai 2020 à 12:41
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Base de données :  `vtc`
+--
+CREATE DATABASE IF NOT EXISTS `vtc` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `vtc`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `association_vehicule_conducteur`
+--
+
+CREATE TABLE `association_vehicule_conducteur` (
+  `id_association` int(11) NOT NULL,
+  `id_vehicule` int(11) NOT NULL,
+  `id_conducteur` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `association_vehicule_conducteur`
+--
+
+INSERT INTO `association_vehicule_conducteur` (`id_association`, `id_vehicule`, `id_conducteur`) VALUES
+(1, 501, 1),
+(2, 502, 2),
+(3, 503, 3),
+(4, 504, 4),
+(5, 501, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `conducteur`
+--
+
+CREATE TABLE `conducteur` (
+  `id_conducteur` int(11) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `conducteur`
+--
+
+INSERT INTO `conducteur` (`id_conducteur`, `prenom`, `nom`) VALUES
+(1, 'Julien', 'Avigny'),
+(2, 'Morgane', 'Alamia'),
+(3, 'Philippe', 'Pandre'),
+(4, 'Amelie', 'Blondelle'),
+(5, 'Alex', 'Richy');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `vehicule`
+--
+
+CREATE TABLE `vehicule` (
+  `id_vehicule` int(11) NOT NULL,
+  `marque` varchar(255) NOT NULL,
+  `modele` varchar(255) NOT NULL,
+  `couleur` varchar(255) NOT NULL,
+  `immatriculation` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `vehicule`
+--
+
+INSERT INTO `vehicule` (`id_vehicule`, `marque`, `modele`, `couleur`, `immatriculation`) VALUES
+(501, 'Peugeot', '807', 'noir', 'AB-355-CA'),
+(502, 'Citroen', 'C8', 'bleu', 'CE-122-AE'),
+(503, 'Mercedes', 'Cls', 'vert', 'FG-953-HI'),
+(504, 'Volkswagen', 'Touran', 'noir', 'SO-322-NV'),
+(505, 'Skoda', 'Octavia', 'gris', 'PB-631-TK'),
+(506, 'Volkswagen', 'Passat', 'gris', 'XN-973-MM');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `association_vehicule_conducteur`
+--
+ALTER TABLE `association_vehicule_conducteur`
+  ADD PRIMARY KEY (`id_association`),
+  ADD KEY `id_vehicule` (`id_vehicule`),
+  ADD KEY `id_conducteur` (`id_conducteur`);
+
+--
+-- Index pour la table `conducteur`
+--
+ALTER TABLE `conducteur`
+  ADD PRIMARY KEY (`id_conducteur`);
+
+--
+-- Index pour la table `vehicule`
+--
+ALTER TABLE `vehicule`
+  ADD PRIMARY KEY (`id_vehicule`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `association_vehicule_conducteur`
+--
+ALTER TABLE `association_vehicule_conducteur`
+  MODIFY `id_association` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `conducteur`
+--
+ALTER TABLE `conducteur`
+  MODIFY `id_conducteur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT pour la table `vehicule`
+--
+ALTER TABLE `vehicule`
+  MODIFY `id_vehicule` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=508;
